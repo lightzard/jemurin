f78ee69088eb16f33cb4c1b85e7042baa88dba60
lightzard <irfan.nur.95@gmail.com>
2015-03-08 10:15:12 +0700

    haha

---
+++ haha.php
@@ -0,0 +1,14 @@
+<form method="POST" action='http://www.sunsquarestudio.com/jemurin/set_rain.php'>
+<input type="submit" name="button1"  value="Hujan">
+</form>
+
+
+<form method="POST" action='http://www.sunsquarestudio.com/jemurin/set_clear.php'>
+<input type="submit" name="button1"  value="Terang">
+</form>
+
+
+
+<form method="POST" action='http://www.sunsquarestudio.com/jemurin/set_clear.php'>
+<input type="submit" name="button1"  value="Selesai">
+</form>
